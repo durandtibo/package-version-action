dc3642a14a392a62cb8b32b6b73808cc99a9a5d6
Thibaut Durand <durand.tibo@gmail.com>
2024-09-11 15:46:09 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ const core = require('@actions/core')
 
 try {
   const pkgName = core.getInput('package-name')
-  console.log(`pkgName: ${pkgName}`)
   const pkgVersion = core.getInput('package-version')
-  console.log(`pkgVersion: ${pkgVersion}`)
   const pythonVersion = core.getInput('python-version')
-  console.log(`pythonVersion: ${pythonVersion}`)
+  console.log(
+    `pkgName: ${pkgName}\npkgVersion: ${pkgVersion}\npythonVersion: ${pythonVersion}`
+  )
+
+  core.setOutput('closest-valid-version', pkgVersion)
 
   const time = new Date().toTimeString()
   core.setOutput('time', time)
