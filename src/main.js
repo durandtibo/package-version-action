9c486bf79de8c2ab76cc2410ac0e9af16901f0ee
Thibaut Durand <durand.tibo@gmail.com>
2024-09-11 15:41:37 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 const core = require('@actions/core')
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet')
-  console.log(`Hello ${nameToGreet}!`)
-
   const pkgName = core.getInput('package-name')
   console.log(`pkgName: ${pkgName}`)
   const pkgVersion = core.getInput('package-version')
