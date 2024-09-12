d0fcf44f850e4b6ba405c04a6ed67dd5aa537100
Thibaut Durand <durand.tibo@gmail.com>
2024-09-11 17:08:07 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import findClosestVersion from './validation.js'
-
 const core = require('@actions/core')
+
+const deps = new Map()
+deps.set(
+  'numpy',
+  new Map([
+    ['3.12', ['1.26.0', null]],
+    ['3.11', ['1.23.2', null]],
+    ['3.10', ['1.21.3', null]],
+    ['3.9', ['1.19.3', '2.0.2']]
+  ])
+)
+
+function findClosestVersion(pkgName, pkgVersion, pythonVersion) {
+  console.log(`${deps}`)
+  return pkgVersion
+}
 
 try {
   const pkgName = core.getInput('package-name')
