9b9d9982d6a4c4b2b6dd52e25248a9da9c129929
Thibaut Durand <durand.tibo@gmail.com>
2024-09-11 23:48:25 -0700

    Add function to find closest version (#6)

---
+++ __tests__/main.test.js
@@ -0,0 +1,234 @@
+const core = require('@actions/core')
+const main = require('../src/main')
+
+// Mock the GitHub Actions core library
+const getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+const setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
+const setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+// Mock the action's main function
+const runMock = jest.spyOn(main, 'run')
+
+allConfigMock = new Map([
+  [
+    'my_package',
+    new Map([
+      ['3.12', ['1.5.0', null]],
+      ['3.11', ['1.2.0', '1.8.0']],
+      ['3.10', [null, '1.3.0']],
+      ['3.9', [null, null]]
+    ])
+  ]
+])
+
+//////////////////////////
+// Tests for findConfig //
+//////////////////////////
+
+test('find config empty', () => {
+  expect(main.findConfig('my_package', '3.11', new Map())).toStrictEqual([
+    null,
+    null
+  ])
+})
+
+test('find config missing package', () => {
+  expect(
+    main.findConfig('my_package', '3.11', new Map([['numpy', new Map()]]))
+  ).toStrictEqual([null, null])
+})
+
+test('find config missing python version', () => {
+  expect(
+    main.findConfig('my_package', '3.11', new Map([['my_package', new Map()]]))
+  ).toStrictEqual([null, null])
+})
+
+test('find config exist min', () => {
+  expect(main.findConfig('my_package', '3.12', allConfigMock)).toStrictEqual([
+    '1.5.0',
+    null
+  ])
+})
+
+test('find config exist min and max', () => {
+  expect(main.findConfig('my_package', '3.11', allConfigMock)).toStrictEqual([
+    '1.2.0',
+    '1.8.0'
+  ])
+})
+
+test('find config exist max', () => {
+  expect(main.findConfig('my_package', '3.10', allConfigMock)).toStrictEqual([
+    null,
+    '1.3.0'
+  ])
+})
+
+test('find config exist null', () => {
+  expect(main.findConfig('my_package', '3.9', allConfigMock)).toStrictEqual([
+    null,
+    null
+  ])
+})
+
+//////////////////////////////////
+// Tests for findClosestVersion //
+//////////////////////////////////
+
+test('find closest version empty', () => {
+  expect(
+    main.findClosestVersion('my_package', '1.2.3', '3.11', new Map())
+  ).toStrictEqual('1.2.3')
+})
+
+test('find closest version min valid', () => {
+  expect(
+    main.findClosestVersion('my_package', '1.7.3', '3.12', allConfigMock)
+  ).toStrictEqual('1.7.3')
+})
+
+test('find closest version min invalid', () => {
+  expect(
+    main.findClosestVersion('my_package', '1.2.3', '3.12', allConfigMock)
+  ).toStrictEqual('1.5.0')
+})
+
+test('find closest version min and max valid', () => {
+  expect(
+    main.findClosestVersion('my_package', '1.5.0', '3.11', allConfigMock)
+  ).toStrictEqual('1.5.0')
+})
+
+test('find closest version min and max invalid lower', () => {
+  expect(
+    main.findClosestVersion('my_package', '1.0.3', '3.11', allConfigMock)
+  ).toStrictEqual('1.2.0')
+})
+
+test('find closest version min and max invalid higher', () => {
+  expect(
+    main.findClosestVersion('my_package', '2.0.3', '3.11', allConfigMock)
+  ).toStrictEqual('1.8.0')
+})
+
+test('find closest version max valid', () => {
+  expect(
+    main.findClosestVersion('my_package', '1.2.3', '3.10', allConfigMock)
+  ).toStrictEqual('1.2.3')
+})
+
+test('find closest version max invalid', () => {
+  expect(
+    main.findClosestVersion('my_package', '1.7.3', '3.10', allConfigMock)
+  ).toStrictEqual('1.3.0')
+})
+
+test('find closest version null', () => {
+  expect(
+    main.findClosestVersion('my_package', '1.2.3', '3.9', allConfigMock)
+  ).toStrictEqual('1.2.3')
+})
+
+////////////////////////
+// Tests for printMap //
+////////////////////////
+
+test('print empty map', () => {
+  main.printMap(new Map())
+})
+
+test('print map', () => {
+  main.printMap(
+    new Map([
+      ['3.12', ['1.5.0', null]],
+      ['3.11', ['1.2.0', '1.8.0']],
+      ['3.10', [null, '1.3.0']],
+      ['3.9', [null, null]]
+    ])
+  )
+})
+
+test('print nested map', () => {
+  main.printMap(allConfigMock)
+})
+
+///////////////////
+// Tests for run //
+///////////////////
+
+describe('action', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('check numpy 2.0.2 and python 3.11', async () => {
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'package-name':
+          return 'numpy'
+        case 'package-version':
+          return '2.0.2'
+        case 'python-version':
+          return '3.11'
+        default:
+          return ''
+      }
+    })
+
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    expect(setOutputMock).toHaveBeenNthCalledWith(
+      1,
+      'closest-valid-version',
+      '2.0.2'
+    )
+    expect(setOutputMock).toHaveBeenNthCalledWith(2, 'is-valid-version', true)
+  })
+
+  it('check numpy 1.10.0 and python 3.11', async () => {
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'package-name':
+          return 'numpy'
+        case 'package-version':
+          return '1.10.0'
+        case 'python-version':
+          return '3.11'
+        default:
+          return ''
+      }
+    })
+
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    expect(setOutputMock).toHaveBeenNthCalledWith(
+      1,
+      'closest-valid-version',
+      '1.23.2'
+    )
+    expect(setOutputMock).toHaveBeenNthCalledWith(2, 'is-valid-version', false)
+  })
+
+  it('fails if no input is provided', async () => {
+    // Set the action's inputs as return values from core.getInput()
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'package-name':
+          throw new Error('Input required and not supplied')
+        default:
+          return ''
+      }
+    })
+
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    // Verify that all of the core library functions were called correctly
+    expect(setFailedMock).toHaveBeenNthCalledWith(
+      1,
+      'Input required and not supplied'
+    )
+  })
+})
--- src/main.js
+++ src/main.js
@@ -1,17 +1,92 @@
 const core = require('@actions/core')
+const semver = require('semver')
 
-try {
-  const pkgName = core.getInput('package-name')
-  const pkgVersion = core.getInput('package-version')
-  const pythonVersion = core.getInput('python-version')
-  console.log(
-    `pkgName: ${pkgName}\npkgVersion: ${pkgVersion}\npythonVersion: ${pythonVersion}`
+// Extra argument for printing with indentation
+function printMap(map, tab = '') {
+  for (const [k, v] of map.entries()) {
+    // Don't print here yet...
+    if (v instanceof Map) {
+      console.log(`${tab}${k}:`) // Only print the key here...
+      printMap(v, tab + '    ') // ...as recursion will take care of the value(s)
+    } else {
+      console.log(`${tab}${k} = ${v}`)
+    }
+  }
+}
+
+const deps = new Map()
+deps.set(
+  'numpy',
+  new Map([
+    ['3.12', ['1.26.0', null]],
+    ['3.11', ['1.23.2', null]],
+    ['3.10', ['1.21.3', null]],
+    ['3.9', ['1.19.3', '2.0.2']]
+  ])
+)
+
+function findConfig(pkgName, pythonVersion, allConfigs) {
+  if (!allConfigs.has(pkgName)) {
+    return [null, null]
+  }
+  const pkgConfig = allConfigs.get(pkgName)
+  if (!pkgConfig.has(pythonVersion)) {
+    return [null, null]
+  }
+  return pkgConfig.get(pythonVersion)
+}
+
+function findClosestVersion(pkgName, pkgVersion, pythonVersion, allConfigs) {
+  const [minVersion, maxVersion] = findConfig(
+    pkgName,
+    pythonVersion,
+    allConfigs
   )
+  console.log(`${minVersion}  ${maxVersion}`)
+  if (minVersion === null && maxVersion === null) {
+    return pkgVersion
+  }
+  if (minVersion === null) {
+    return semver.lte(pkgVersion, maxVersion) ? pkgVersion : maxVersion
+  }
+  if (maxVersion === null) {
+    return semver.gte(pkgVersion, minVersion) ? pkgVersion : minVersion
+  }
+  if (semver.gt(pkgVersion, maxVersion)) {
+    return maxVersion
+  }
+  if (semver.lt(pkgVersion, minVersion)) {
+    return minVersion
+  }
+  return pkgVersion
+}
 
-  core.setOutput('closest-valid-version', pkgVersion)
+async function run() {
+  try {
+    const pkgName = core.getInput('package-name')
+    const pkgVersion = core.getInput('package-version')
+    const pythonVersion = core.getInput('python-version')
+    console.log(
+      `pkgName: ${pkgName}\npkgVersion: ${pkgVersion}\npythonVersion: ${pythonVersion}`
+    )
 
-  const time = new Date().toTimeString()
-  core.setOutput('time', time)
-} catch (error) {
-  core.setFailed(error.message)
+    closestPkgVersion = findClosestVersion(
+      pkgName,
+      pkgVersion,
+      pythonVersion,
+      deps
+    )
+    console.log(`closestPkgVersion ${closestPkgVersion}`)
+    core.setOutput('closest-valid-version', closestPkgVersion)
+    core.setOutput('is-valid-version', closestPkgVersion === pkgVersion)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+module.exports = {
+  findConfig: findConfig,
+  findClosestVersion: findClosestVersion,
+  printMap: printMap,
+  run: run
 }
